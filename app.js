55615d8725bd11d0bb4a613dcdf6e41b349b3b0a
gintokid <juventino1802@gmail.com>
2023-12-23 22:05:47 +0100

    added cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
 const sequelize = require('./src/db/sequelize')
+const cors = require('cors')
 
 // instantion express et ajouter les middleware
 const app = express()
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3000
 app
     .use(favicon(__dirname + '/favicon.ico'))
     .use(bodyParser.json())
+    .use(cors())
 
 
 app.get('/', (req, res) => {
